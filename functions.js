ad4ecd4f81dadb1f0557bd4ff65e2629b3523724
Ahmed Abd El-Latif <46932157+AhmedAbdoDev@users.noreply.github.com>
2025-02-02 17:12:10 +0200

    Deploying

--- functions.js
+++ functions.js
@@ -1,14 +1,22 @@
 module.exports = {
   validatePost: async (req, res, next) => {
     const { title, content } = req.body;
-    if (!title || !content) {
+    if (!title || !content)
       return res.status(400).json({ error: "Title and content are required." });
-    }
-    if (content.length < 20) {
+    if (content.length < 20)
       return res
         .status(400)
         .json({ error: "Content must be at least 20 characters long." });
-    }
+    next();
+  },
+  validateComment: async (req, res, next) => {
+    const { content } = req.body;
+    if (!content?.length)
+      return res.status(400).json({ error: "Content is required." });
+    if (content.length < 2)
+      return res
+        .status(400)
+        .json({ error: "Content must be at least 2 characters long." });
     next();
   },
 };
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Database/models/user");
-const { validatePost } = require("../functions");
+const { validatePost, validateComment } = require("../functions");
+const authMiddleware = require("../middlewares/authMiddleware");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/users", async (req, res) => {
   try {
@@ -12,60 +15,89 @@ router.post("/users", async (req, res) => {
     if (existingUser)
       return res.status(400).json({
         message: "Username or email already exists. Please choose another.",
+        success: false,
       });
     const user = new User({ username, email, password });
     await user.save();
     res.status(201).json({
       message: "User created successfully",
       user: { id: user._id, username: user.username },
+      success: true,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-router.post("/users/:userId/posts", validatePost, async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const { title, content } = req.body;
-    let user = await User.findOne({ _id: userId, "posts.title": title });
-    if (user)
-      return res
-        .status(400)
-        .json({ error: "A post with the same title already exists." });
-    user = await User.findByIdAndUpdate(
-      userId,
-      { $push: { posts: { title, content } } },
-      { new: true }
-    ).select("username posts");
-    if (!user) return res.status(404).json({ message: "User not found" });
-    res.status(201).json({ message: "Post added successfully", user });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-router.post("/users/:userId/posts/:postId/comments", async (req, res) => {
-  try {
-    const { userId, postId } = req.params;
-    const { user, content } = req.body;
 
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId, "posts._id": postId },
-      { $push: { "posts.$.comments": { user, content } } },
-      { new: true }
-    ).select("username posts");
-    if (!updatedUser)
-      return res.status(404).json({ message: "User or Post not found" });
-    res
-      .status(201)
-      .json({ message: "Comment added successfully", updatedUser });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !bcrypt.compareSync(password, user?.password))
+      return res.status(400).json({ message: "Invalid email or password" });
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.json({ message: "Login successful", token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-router.get("/users/:userId/posts", async (req, res) => {
+
+router.post(
+  "/users/:userId/posts",
+  [authMiddleware, validatePost],
+  async (req, res) => {
+    try {
+      const { userId } = req.params;
+      if (req.user.id !== userId)
+        return res.status(403).json({ message: "Unauthorized" });
+      const { title, content } = req.body;
+      let user = await User.findOne({ _id: userId, "posts.title": title });
+      if (user)
+        return res
+          .status(400)
+          .json({ error: "A post with the same title already exists." });
+      user = await User.findByIdAndUpdate(
+        userId,
+        { $push: { posts: { title, content } } },
+        { new: true }
+      ).select("username posts");
+      if (!user) return res.status(404).json({ message: "User not found" });
+      res.status(201).json({ message: "Post added successfully", user });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+router.post(
+  "/users/:userId/posts/:postId/comments",
+  [authMiddleware, validateComment],
+  async (req, res) => {
+    try {
+      const { userId, postId } = req.params;
+      const { content } = req.body;
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId, "posts._id": postId },
+        { $push: { "posts.$.comments": { user: req.user.username, content } } },
+        { new: true }
+      ).select("username posts");
+      if (!updatedUser)
+        return res.status(404).json({ message: "User or Post not found" });
+      res
+        .status(201)
+        .json({ message: "Comment added successfully", updatedUser });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+router.get("/users/:userId/posts", authMiddleware, async (req, res) => {
   try {
     const { userId } = req.params;
-    const user = await User.findById(userId).select("username posts");
+    const user = await User.findById(userId).select("posts");
     if (!user) return res.status(404).json({ message: "User not found" });
     res.status(200).json({ posts: user.posts });
   } catch (error) {
